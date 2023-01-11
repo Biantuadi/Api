8a6cd2a94b07a001d30004f10add66054cf8773e
Kevin-beni <benibiantuadi@gmail.com>
2023-01-11 19:26:58 +0100

    log

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(express.urlencoded({ extended: true })); // pour les formulaires
 // db
 // require("./config/db");
 const mongoose = require('mongoose');
-const password = "1uhYc2Qyjid4tzgE";
-const username = "KBB";
-const dbName = "Etna_api";
+const password = process.env.MONGODB_PASSWORD;
+const username = process.env.MONGODB_USERNAME;
+const dbName = process.env.MONGODB_DBNAME;
 const url = `mongodb+srv://${username}:${password}@cluster0.y0gnr7k.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
  
