5b79e993f54054622a1e60d4a6b3138f4667a517
Kevin-beni <benibiantuadi@gmail.com>
2023-01-11 20:38:14 +0100

    cors test

--- app.js
+++ app.js
@@ -11,16 +11,26 @@ require("dotenv").config();
 //   next();
 // });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "POST , GET , OPTIONS , PUT , DELETE");
+
+  // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Authorization"
+    "X-Requested-With,content-type"
   );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
   next();
 });
 
@@ -29,7 +39,6 @@ app.use(express.urlencoded({ extended: true })); // pour les formulaires
 
 // db
 require("./config/db");
-
 
 // routes
 const userRouter = require("./routes/user.router");
