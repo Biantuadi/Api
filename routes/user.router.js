4987464eb3bd122c22a6712b0b3e87d57d96644a
Kevin-beni <benibiantuadi@gmail.com>
2023-01-11 18:49:33 +0100

    test 2

--- routes/user.router.js
+++ routes/user.router.js
@@ -6,7 +6,7 @@ const auth = require("../middlewares/auth");
 
 router.post("/register", userCtrl.register);
 router.post("/login", userCtrl.login);
-router.get("/users",  userCtrl.users);
+router.get("/users", auth, userCtrl.users);
 router.get("/user/:id", auth, userCtrl.user);
 router.put("/user/:id", auth, userCtrl.update);
 router.delete("/user/:id", auth, userCtrl.delete);
