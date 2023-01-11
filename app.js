fdb0e023dfd1c88cfef5244b5788ecf47dc3428d
Kevin-beni <benibiantuadi@gmail.com>
2023-01-11 18:45:33 +0100

    test

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+  
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // pour les formulaires
 
